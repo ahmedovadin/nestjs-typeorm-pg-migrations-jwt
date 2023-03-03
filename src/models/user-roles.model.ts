1954e6b1549f74ff226820abce3c1e071ea16fc3
Dina <ahmedovadin@gmail.com>
2023-03-03 15:20:37 +0600

    Update user-roles.model.ts

--- src/models/user-roles.model.ts
+++ src/models/user-roles.model.ts
@@ -1,18 +1,13 @@
 
 import { ApiProperty } from "@nestjs/swagger";
-// import { Model, Column, DataType, Table, BelongsToMany, ForeignKey } from "sequelize-typescript";
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinTable} from "typeorm"
 import { User } from "../models/users.model";
 import { Role } from "../models/roles.model";
 
-// @Entity({name: 'user-roles', createdAt: false, updatedAt: false})
 @Entity({name: 'user-roles'})
 export class UserRoles {
     @ApiProperty({example: '1', description: `Unique id`})
     @PrimaryGeneratedColumn('increment')
-    // @Column({
-    //     unique: true, autoIncrement: true, primaryKey: true
-    // })
     id: number;
 
     @ManyToOne(() => Role, role => role.id, {
